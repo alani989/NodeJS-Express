8b2e82cfc5daf8379cdd792ba8abbaa04cdd3db4
Abdullah <abdullah.alwaheb@gmail.com>
2018-03-21 14:55:39 -0500

    updated

--- Exercise/run.js
+++ Exercise/run.js
@@ -23,10 +23,41 @@ app.get('/greeting/:user', function (req, res) {
 // query parameters
 app.get('/year', function (req, res) {
     var currentYear = new Date().getFullYear(); //to get the current year
-    var age = currentYear - (req.query.age*1); //to convert req.query.age into a number we multiplied by 1
+    var age = currentYear - (req.query.age * 1); //to convert req.query.age into a number we multiplied by 1
     res.send("You were born in " + age);
 });
-
+//reading a JSON file
+var dataFile = require('./data.json');
+app.get('/speaker/:speakerid', function (req, res) {
+    var speaker = dataFile.speakers[req.params.speakerid];
+    res.send(
+        `
+        <h1>${speaker.title}</h1>
+        <h2>${speaker.name}</h2>
+        <p>${speaker.shortname}</p>
+        <p>${speaker.summary}</p>
+        <p>${speaker.description}</p>
+        `
+    ); //end of res.send
+});
+//reading a JSON file 2 using a loop
+var dataFile = require('./data.json');
+app.get('/speaker', function (req, res) {
+    var info = '';
+    dataFile.speakers.forEach(function (speaker) {
+        info +=
+            `
+            <h1>${speaker.title}</h1>
+            <h2>${speaker.name}</h2>
+            <p>${speaker.shortname}</p>
+            <p>${speaker.summary}</p>
+            <p>${speaker.description}</p>
+            `
+    });
+    res.send(
+        `${info}`
+    );
+});
 // initialize and test connection
 app.listen(5000, function () {
     console.log('Connected to port 5000');
--- TestProject/run.js
+++ TestProject/run.js
@@ -1,10 +1,35 @@
-var request = require('request');
-var url = 'https://nodejs.org/en/';
-request.get(url, function (error, response, html) {
-  if (error) {
-    console.error(error.message);
-    return;
-  }
-  console.log(html);
-});
+// // With pure NodeJS 
+// var http = require('http');
+// var myServer = http.createServer(function(request, response){
+//     response.writeHead(200, {"Content-Type": "text/plain"}); plain could be changed to HTML
+//     //to send back something to client
+//     response.write('Digital Crafts'); //if this was response.write('<h1>Digital Crafts'</h1>) then plain should be changed to HTML
+//     response.end();
+// });
+// //asking server to  to listen on  a specific port
+// myServer.listen(3000);
 
+
+// with Express
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+app.get('/home', function(req, res) {
+    res.send('This is Home!');
+});
+app.get('/about', function(req, res) {
+    res.send('About us!');
+});
+// route params example
+app.get('/hello/:name', function(req, res){
+    var name = req.params.name;
+    res.send('Hello ' + name + '!');
+});
+// 
+
+// display a message that verifies connection
+app.listen(3000, function(){
+    console.log('Example app listening on port 3000!');
+});
