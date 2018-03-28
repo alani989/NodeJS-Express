ae746ddeb14aa7bc60902491a361cc39ec05992d
Abdullah <abdullah.alwaheb@gmail.com>
2018-03-28 12:23:41 -0500

    updated

--- ChatApp/run.js
+++ ChatApp/run.js
@@ -1,28 +1,19 @@
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
-    });
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-io.emit('some event', { for: 'everyone' });
-
-//   Each socket also fires a special disconnect event:
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
-
-
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
